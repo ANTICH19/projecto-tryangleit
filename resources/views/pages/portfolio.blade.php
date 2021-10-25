8b18bddee126249d8057aa612c48b843c6623af9
WIN21H1 <ivens.labrador.t0119172@edu.atec.pt>
2021-10-25 19:41:57 +0100

    v.25.10 - Ivens

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -20,9 +20,14 @@
             @endcomponent
         </div>
     </header>
-    <div class="container d-flex justify-content-center" style="padding-top:15%">
-        <img src="{{'images/tryangleitWhite.png'}}" alt="logo">
-    </div>
+
+        <picture  class="d-flex justify-content-center" >
+            <source media="(min-width:771px)" srcset="{{asset('images/tryangleitWhite.png')}}">
+            <source media="(min-width:0px)" srcset="{{asset('images/yTryangleitHome.png')}}">
+            <img id="imag-home" src="{{asset('images/tryangleitWhite.png')}}">
+        </picture>
+
+
 </div>
     <div class="container" style="padding-top: 5%;padding-bottom: 2%">
         <div class="row mt-3">
@@ -38,8 +43,7 @@
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
-        </div>
-        <div class="row mt-3">
+
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
@@ -52,8 +56,7 @@
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
-        </div>
-        <div class="row mt-3">
+
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
@@ -66,8 +69,7 @@
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
-        </div>
-        <div class="row mt-3">
+
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="margin-bottom: 10px;">
                 <img class="img-fluid" style="border-radius: 7%" src="https://mcdn.wallpapersafari.com/medium/77/90/056lrX.jpg" alt="">
             </div>
